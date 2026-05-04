80258edabe80f49173799e56369d49870b13206d
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-04 18:31:54 +0530

    Add delta unit test

--- tests/App_Code/CookieManagerTests.cs
+++ tests/App_Code/CookieManagerTests.cs
@@ -9,22 +9,45 @@ namespace OWASP.WebGoat.NET.App_Code.Tests
     public class CookieManagerTests
     {
         [Fact]
-        public void SetCookie_Called_SetsHttpOnlyAndSecureFlags()
+        public void SetCookie_SetsHttpOnlyAndSecureFlags_AlwaysTrue()
         {
             // Arrange
             var ticket = new FormsAuthenticationTicket(
-                version: 1,
-                name: "user",
-                issueDate: DateTime.UtcNow,
-                expiration: DateTime.UtcNow.AddMinutes(30),
-                isPersistent: false,
-                userData: "data");
+                1,
+                "user",
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(5),
+                false,
+                "userdata");
 
             // Act
-            HttpCookie cookie = CookieManager.SetCookie(ticket, "ignoredId", "ignoredValue");
+            HttpCookie cookie = CookieManager.SetCookie(ticket, "id", "value");
 
             // Assert
             Assert.NotNull(cookie);
+            Assert.True(cookie.HttpOnly);
+            Assert.True(cookie.Secure);
+        }
+
+        [Fact]
+        public void SetCookie_PersistentTicket_SetsExpiresToTicketExpiration()
+        {
+            // Arrange
+            var expiration = DateTime.UtcNow.AddHours(1);
+            var ticket = new FormsAuthenticationTicket(
+                1,
+                "user",
+                DateTime.UtcNow,
+                expiration,
+                true,
+                "userdata");
+
+            // Act
+            HttpCookie cookie = CookieManager.SetCookie(ticket, "id", "value");
+
+            // Assert
+            Assert.NotNull(cookie);
+            Assert.Equal(expiration, cookie.Expires);
             Assert.True(cookie.HttpOnly);
             Assert.True(cookie.Secure);
         }

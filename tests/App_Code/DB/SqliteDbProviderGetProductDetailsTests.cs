5fd2ee438096368b717136abb4a888771886ca0f
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-06 19:01:44 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderGetProductDetailsTests.cs
+++ tests/App_Code/DB/SqliteDbProviderGetProductDetailsTests.cs
@@ -1,22 +1,27 @@
+using System;
+using Moq;
 using OWASP.WebGoat.NET.App_Code.DB;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
-    public class SqliteDbProviderGetProductDetailsTests
+    public class SqliteDbProvider_GetProductDetails_Tests
     {
         [Fact]
-        public void GetProductDetails_UsesParameterPlaceholder_ForProductCode()
+        public void GetProductDetails_WhenGivenInjectionLikeProductCode_DoesNotTreatAsSqlFragment()
         {
-            // Arrange/Act
-            var productsSql = "select * from Products where productCode = @productCode";
-            var commentsSql = "select * from Comments where productCode = @productCode";
+            // Arrange
+            var config = new Mock<ConfigFile>();
+            config.Setup(c => c.Get(It.IsAny<string>())).Returns(string.Empty);
+            var provider = new SqliteDbProvider(config.Object);
+
+            // Act
+            var payload = "X' OR '1'='1";
+            Exception ex = Record.Exception(() => provider.GetProductDetails(payload));
 
             // Assert
-            Assert.Contains("@productCode", productsSql);
-            Assert.Contains("@productCode", commentsSql);
-            Assert.DoesNotContain("'\" + productCode + \"'", productsSql);
-            Assert.DoesNotContain("'\" + productCode + \"'", commentsSql);
+            if (ex != null)
+                Assert.DoesNotContain(payload, ex.ToString());
         }
     }
 }

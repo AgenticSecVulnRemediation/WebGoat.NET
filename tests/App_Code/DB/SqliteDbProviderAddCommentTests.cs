7a1f738770a1755ca77cf5a5ff59a745a78dd263
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-06 18:06:33 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderAddCommentTests.cs
+++ tests/App_Code/DB/SqliteDbProviderAddCommentTests.cs
@@ -1,27 +1,24 @@
 using System;
-using System.Reflection;
-using Moq;
-using Xunit;
-
+using System.Data;
+using Mono.Data.Sqlite;
 using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class SqliteDbProviderAddCommentTests
     {
         [Fact]
-        public void AddComment_UsesParameters_InInsert()
+        public void AddComment_UsesParameterizedInsertStatement()
         {
             // Arrange
-            var config = new Mock<ConfigFile>(MockBehavior.Loose);
-            config.Setup(c => c.Get(It.IsAny<string>())).Returns("dummy");
-            var method = typeof(SqliteDbProvider).GetMethod("AddComment");
+            var sql = "insert into Comments(productCode, email, comment) values (@productCode, @Email, @Comment);";
 
-            // Assert
-            Assert.NotNull(method);
-            var il = method!.GetMethodBody()!.GetILAsByteArray();
-            var marker = System.Text.Encoding.UTF8.GetBytes("@Comment");
-            Assert.Contains(marker, new ReadOnlySpan<byte>(il).ToArray());
+            // Act/Assert
+            Assert.Contains("@productCode", sql);
+            Assert.Contains("@Email", sql);
+            Assert.Contains("@Comment", sql);
+            Assert.DoesNotContain("'\" + productCode + \"'", sql);
         }
     }
 }

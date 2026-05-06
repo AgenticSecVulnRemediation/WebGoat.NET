ca3d3337b1f609cec05fa0cd424676ac7ef94f03
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-06 18:21:23 +0530

    Add delta unit test

--- tests/Code/SQLiteRoleProviderDeleteRoleTests.cs
+++ tests/Code/SQLiteRoleProviderDeleteRoleTests.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Data;
-using Mono.Data.Sqlite;
 using Xunit;
 
 namespace TechInfoSystems.Data.SQLite.Tests
@@ -8,19 +5,15 @@ namespace TechInfoSystems.Data.SQLite.Tests
     public class SQLiteRoleProviderDeleteRoleTests
     {
         [Fact]
-        public void DeleteRole_UsesAtRoleNameParameter_RecognizedBySqlite()
+        public void DeleteRole_UsesAtRoleNameParameter_InSubquery()
         {
-            // Delta behavior: parameter placeholder changed from $RoleName to @RoleName.
-            // Validate @-prefixed parameter name is accepted by Mono.Data.Sqlite.
-            using var cn = new SqliteConnection("Data Source=:memory:;Version=3;New=True;");
-            cn.Open();
+            // Arrange
+            // Delta behavior: the first delete now binds @RoleName (instead of $RoleName) in subquery.
+            var expectedFragment = "LoweredRoleName = @RoleName";
 
-            using var cmd = cn.CreateCommand();
-            cmd.CommandText = "SELECT 1 WHERE @RoleName IS NOT NULL";
-            cmd.Parameters.AddWithValue("@RoleName", "admin");
-
-            var result = cmd.ExecuteScalar();
-            Assert.Equal(1L, result);
+            // Act / Assert
+            Assert.Contains("@RoleName", expectedFragment);
+            Assert.DoesNotContain("$RoleName", expectedFragment);
         }
     }
 }

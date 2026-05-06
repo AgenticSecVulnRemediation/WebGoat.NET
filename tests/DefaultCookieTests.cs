fbcb865374107737056b4b696bc8d5eaba77d79c
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-06 19:02:37 +0530

    Add delta unit test

---
+++ tests/DefaultCookieTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Web;
+using OWASP.WebGoat.NET;
+using Xunit;
+
+namespace OWASP.WebGoat.NET.Tests
+{
+    public class DefaultPageCookieTests
+    {
+        [Fact]
+        public void ServerCookie_IsHttpOnlyAndSecure()
+        {
+            // Arrange
+            var cookie = new HttpCookie("Server", "value");
+
+            // Act
+            cookie.HttpOnly = true;
+            cookie.Secure = true;
+
+            // Assert
+            Assert.True(cookie.HttpOnly);
+            Assert.True(cookie.Secure);
+        }
+    }
+}

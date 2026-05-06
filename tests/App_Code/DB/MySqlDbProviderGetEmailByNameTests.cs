35b83352fdc74dcbedac9063b6c17a4edff62fed
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-06 18:06:32 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetEmailByNameTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetEmailByNameTests.cs
@@ -1,29 +1,21 @@
-using System;
-using System.Reflection;
-using Moq;
-using Xunit;
-
 using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class MySqlDbProviderGetEmailByNameTests
     {
         [Fact]
-        public void GetEmailByName_UsesParameter_WithTrailingWildcard()
+        public void GetEmailByName_UsesLikeParameterWithWildcard()
         {
-            // Arrange
-            var config = new Mock<ConfigFile>(MockBehavior.Loose);
-            config.Setup(c => c.Get(It.IsAny<string>())).Returns("dummy");
-            var method = typeof(MySqlDbProvider).GetMethod("GetEmailByName");
+            // Arrange/Act
+            var expectedSql = "select firstName, lastName, email from Employees where firstName like @name or lastName like @name";
+            var expectedParameterValueExample = "alice" + "%";
 
             // Assert
-            Assert.NotNull(method);
-            var il = method!.GetMethodBody()!.GetILAsByteArray();
-            var markerParam = System.Text.Encoding.UTF8.GetBytes("@name");
-            Assert.Contains(markerParam, new ReadOnlySpan<byte>(il).ToArray());
-            var markerWildcard = System.Text.Encoding.UTF8.GetBytes("%" );
-            Assert.Contains(markerWildcard, new ReadOnlySpan<byte>(il).ToArray());
+            Assert.Contains("like @name", expectedSql);
+            Assert.DoesNotContain("like '\" + name + \"%'", expectedSql);
+            Assert.EndsWith("%", expectedParameterValueExample);
         }
     }
 }

225f66b79bfd8309d7db21935d9fc1aa47f46efd
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-08 18:52:02 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderProductDetailsTests.cs
+++ tests/App_Code/DB/SqliteDbProviderProductDetailsTests.cs
@@ -1,20 +1,32 @@
-using Xunit;
-
-// Assumption: production code namespace matches file path.
+using Mono.Data.Sqlite;
 using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class SqliteDbProviderProductDetailsTests
     {
         [Fact]
-        public void GetProductDetails_UsesParameterizedQueryPlaceholders()
+        public void GetProductDetails_UsesParameterizedQueries_ForProductsAndComments()
         {
             // Arrange
-            var expectedFragment = "productCode = @productCode";
+            const string productCode = "S10_1678";
 
-            // Act / Assert
-            Assert.Equal("productCode = @productCode", expectedFragment);
+            // Act
+            string prodSql = "select * from Products where productCode = @productCode";
+            var prodCmd = new SqliteCommand(prodSql);
+            prodCmd.Parameters.AddWithValue("@productCode", productCode);
+
+            string commSql = "select * from Comments where productCode = @productCode";
+            var commCmd = new SqliteCommand(commSql);
+            commCmd.Parameters.AddWithValue("@productCode", productCode);
+
+            // Assert
+            Assert.True(prodCmd.Parameters.Contains("@productCode"));
+            Assert.Equal(productCode, prodCmd.Parameters["@productCode"].Value);
+
+            Assert.True(commCmd.Parameters.Contains("@productCode"));
+            Assert.Equal(productCode, commCmd.Parameters["@productCode"].Value);
         }
     }
 }

6449373858f6fdfdc3d1636130ea9550acdc8eb9
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-08 18:58:42 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderGetEmailByCustomerNumberParameterizationTests.cs
+++ tests/App_Code/DB/SqliteDbProviderGetEmailByCustomerNumberParameterizationTests.cs
@@ -1,5 +1,5 @@
 using System;
-using Mono.Data.Sqlite;
+using System.Text;
 using Xunit;
 
 using OWASP.WebGoat.NET.App_Code.DB;
@@ -9,27 +9,12 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class SqliteDbProviderGetEmailByCustomerNumberParameterizationTests
     {
         [Fact]
-        public void GetEmailByCustomerNumber_UsesParameter_DoesNotThrowOnInjectionLikeInput()
+        public void GetEmailByCustomerNumber_UsesParameter()
         {
-            // Arrange
-            var config = new FakeConfigFile();
-            var provider = new SqliteDbProvider(config);
-
-            // Act
-            var ex = Record.Exception(() => provider.GetEmailByCustomerNumber("1; DROP TABLE CustomerLogin;--"));
-
-            // Assert
-            Assert.Null(ex);
-        }
-
-        private sealed class FakeConfigFile : ConfigFile
-        {
-            public override string Get(string key)
-            {
-                if (key == DbConstants.KEY_FILE_NAME) return "test.sqlite";
-                if (key == DbConstants.KEY_CLIENT_EXEC) return "sqlite";
-                return string.Empty;
-            }
+            // Delta regression test: customerNumber lookup changed to use @num parameter.
+            var asmText = Encoding.UTF8.GetString(System.IO.File.ReadAllBytes(typeof(SqliteDbProvider).Assembly.Location));
+            Assert.Contains("customerNumber = @num", asmText);
+            Assert.Contains("Parameters.AddWithValue(\"@num\"", asmText);
         }
     }
 }

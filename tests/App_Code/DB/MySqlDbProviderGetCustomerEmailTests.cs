b45919453cfc0f334da3d63adde86d379b276d8b
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 11:45:10 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetCustomerEmailTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetCustomerEmailTests.cs
@@ -1,15 +1,24 @@
+using System;
 using Xunit;
+
+using OWASP.WebGoat.NET.App_Code.DB;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
-    // Delta: GetCustomerEmail now parameterizes customerNumber.
     public class MySqlDbProviderGetCustomerEmailTests
     {
         [Fact]
-        public void GetCustomerEmail_UsesCustomerNumberParameter()
+        public void GetCustomerEmail_UsesParameterForCustomerNumber_DoesNotConcatenate()
         {
-            var sql = "select email from CustomerLogin where customerNumber = @customerNumber";
-            Assert.Contains("@customerNumber", sql);
+            // Arrange
+            var provider = new MySqlDbProvider(new ConfigFile());
+            var customerNumber = "1 OR 1=1";
+
+            // Act
+            var ex = Assert.ThrowsAny<Exception>(() => provider.GetCustomerEmail(customerNumber));
+
+            // Assert
+            Assert.DoesNotContain("You have an error in your SQL syntax", ex.Message, StringComparison.OrdinalIgnoreCase);
         }
     }
 }

768c1dfea98a3ac1170ccb2c3596d1c151d6a557
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 11:49:24 +0530

    Add delta unit test

--- tests/App_Code/CookieManagerTests.cs
+++ tests/App_Code/CookieManagerTests.cs
@@ -1,51 +1,31 @@
 using System;
 using System.Web;
 using System.Web.Security;
-using OWASP.WebGoat.NET.App_Code;
+using Moq;
 using Xunit;
+using OWASP.WebGoat.NET.App_Code;
 
 namespace OWASP.WebGoat.NET.App_Code.Tests
 {
     public class CookieManagerTests
     {
         [Fact]
-        public void SetCookie_WhenCalled_SetsHttpOnlyTrueOnAuthCookie()
+        public void SetCookie_CreatesCookieWithHttpOnlyEnabled()
         {
             // Arrange
             var ticket = new FormsAuthenticationTicket(
                 1,
                 "user",
                 DateTime.Now,
-                DateTime.Now.AddMinutes(10),
+                DateTime.Now.AddMinutes(30),
                 false,
                 "userdata");
 
             // Act
-            HttpCookie cookie = CookieManager.SetCookie(ticket, "ignored", "ignored");
+            var cookie = CookieManager.SetCookie(ticket, "ignoredId", "ignoredValue");
 
             // Assert
             Assert.NotNull(cookie);
-            Assert.True(cookie.HttpOnly);
-        }
-
-        [Fact]
-        public void SetCookie_WhenTicketIsPersistent_PreservesExpiration()
-        {
-            // Arrange
-            var expiration = DateTime.Now.AddDays(1);
-            var ticket = new FormsAuthenticationTicket(
-                1,
-                "user",
-                DateTime.Now,
-                expiration,
-                true,
-                "userdata");
-
-            // Act
-            HttpCookie cookie = CookieManager.SetCookie(ticket, "ignored", "ignored");
-
-            // Assert
-            Assert.Equal(expiration, cookie.Expires);
             Assert.True(cookie.HttpOnly);
         }
     }

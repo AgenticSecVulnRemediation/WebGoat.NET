4f808c691114bb4a40d1518cf6c2cd5589b0c999
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 11:39:24 +0530

    Add delta unit test

--- tests/App_Code/CookieManagerSecureFlagsTests.cs
+++ tests/App_Code/CookieManagerSecureFlagsTests.cs
@@ -9,27 +9,28 @@ namespace OWASP.WebGoat.NET.App_Code.Tests
     public class CookieManagerSecureFlagsTests
     {
         [Fact]
-        public void SetCookie_SetsHttpOnlyAndSecure()
+        public void SetCookie_SetsHttpOnlyAndSecure_AndUsesEncryptedTicketAsValue()
         {
-            // Delta test: HttpOnly and Secure flags were added to auth cookie.
-
             // Arrange
             var ticket = new FormsAuthenticationTicket(
                 1,
                 "user",
                 DateTime.UtcNow,
-                DateTime.UtcNow.AddMinutes(10),
-                isPersistent: false,
-                userData: "");
+                DateTime.UtcNow.AddMinutes(5),
+                false,
+                "userdata");
+
+            string expectedEncrypted = FormsAuthentication.Encrypt(ticket);
 
             // Act
             HttpCookie cookie = CookieManager.SetCookie(ticket, "ignored", "ignored");
 
             // Assert
+            Assert.NotNull(cookie);
+            Assert.Equal(FormsAuthentication.FormsCookieName, cookie.Name);
+            Assert.Equal(expectedEncrypted, cookie.Value);
             Assert.True(cookie.HttpOnly);
             Assert.True(cookie.Secure);
-            Assert.Equal(FormsAuthentication.FormsCookieName, cookie.Name);
-            Assert.False(string.IsNullOrWhiteSpace(cookie.Value));
         }
     }
 }

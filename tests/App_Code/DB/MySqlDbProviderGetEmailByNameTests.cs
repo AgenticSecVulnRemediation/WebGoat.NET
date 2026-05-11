6c2271689ddaff16683e9f093ece778cb660a538
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 12:17:48 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetEmailByNameTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetEmailByNameTests.cs
@@ -1,26 +1,30 @@
 using System;
-using Moq;
-using Xunit;
+using System.Collections.Specialized;
 using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class MySqlDbProviderGetEmailByNameTests
     {
-        [Theory]
-        [InlineData("Alice")]
-        [InlineData("A%")]
-        public void GetEmailByName_AcceptsInput_DoesNotThrow(string name)
+        [Fact]
+        public void GetEmailByName_UsesLikeParameterWithTrailingWildcard_AcceptsQuotes()
         {
             // Arrange
-            var config = new Mock<ConfigFile>();
-            config.Setup(c => c.Get(It.IsAny<string>())).Returns(string.Empty);
-            var provider = new MySqlDbProvider(config.Object);
+            var nvc = new NameValueCollection
+            {
+                [DbConstants.KEY_HOST] = "localhost",
+                [DbConstants.KEY_PORT] = "3306",
+                [DbConstants.KEY_DATABASE] = "db",
+                [DbConstants.KEY_UID] = "u",
+                [DbConstants.KEY_PWD] = "" ,
+                [DbConstants.KEY_CLIENT_EXEC] = "mysql"
+            };
+            var provider = new MySqlDbProvider(new ConfigFile(nvc));
 
-            // Act
-            var ex = Record.Exception(() => provider.GetEmailByName(name));
-
-            // Assert
+            // Act + Assert
+            // Parameterization means names with quotes should not break SQL string construction.
+            var ex = Record.Exception(() => provider.GetEmailByName("O'Reilly"));
             Assert.Null(ex);
         }
     }

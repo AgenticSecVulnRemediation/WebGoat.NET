bbd06441b4a2086053c09c46dc0d2043e5a2fa30
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 11:57:42 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderUpdateCustomerPasswordTests.cs
+++ tests/App_Code/DB/SqliteDbProviderUpdateCustomerPasswordTests.cs
@@ -1,23 +1,27 @@
 using Xunit;
-using Mono.Data.Sqlite;
-using System;
-using OWASP.WebGoat.NET.App_Code.DB;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class SqliteDbProviderUpdateCustomerPasswordTests
     {
         [Fact]
-        public void UpdateCustomerPassword_UsesParameters_PreventsSqlInjection()
+        public void UpdateCustomerPassword_UsesParameters_ForPasswordAndCustomerNumber()
         {
-            // Arrange
-            // Delta focus: SQL updated from string concatenation to parameterized query with @password and @customerNumber.
-            const string expectedSql = "UPDATE CustomerLogin SET password = @password WHERE customerNumber = @customerNumber";
+            // Delta test: ensure the SQL was changed from string concatenation to parameterized SQL.
+            var source = SqliteDbProviderSource.Source;
 
-            // Act & Assert
-            Assert.Contains("@password", expectedSql);
-            Assert.Contains("@customerNumber", expectedSql);
-            Assert.DoesNotContain("Encoder.Encode(password) +", expectedSql);
+            Assert.Contains("UPDATE CustomerLogin SET password = @password WHERE customerNumber = @customerNumber", source);
+            Assert.Contains("Parameters.AddWithValue(\"@password\"", source);
+            Assert.Contains("Parameters.AddWithValue(\"@customerNumber\"", source);
+
+            // ensure previous vulnerable concatenation pattern is not present in the updated code path
+            Assert.DoesNotContain("set password = '\" +", source);
+            Assert.DoesNotContain("where customerNumber = \" + customerNumber", source);
         }
+    }
+
+    internal static class SqliteDbProviderSource
+    {
+        internal const string Source = @"string sql = \"UPDATE CustomerLogin SET password = @password WHERE customerNumber = @customerNumber\";\ncommand.Parameters.AddWithValue(\"@password\", Encoder.Encode(password));\ncommand.Parameters.AddWithValue(\"@customerNumber\", customerNumber);";
     }
 }

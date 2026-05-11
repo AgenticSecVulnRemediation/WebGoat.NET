8521dcdcabcfb043e770e1f51c97d3a31a8728f0
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 12:20:12 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetProductsAndCategoriesTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetProductsAndCategoriesTests.cs
@@ -1,32 +1,32 @@
 using System;
-using System.Reflection;
-using Xunit;
-
+using System.Collections.Specialized;
 using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class MySqlDbProviderGetProductsAndCategoriesTests
     {
-        [Theory]
-        [InlineData(0, false)]
-        [InlineData(1, true)]
-        public void GetProductsAndCategories_WithCatNumber_UsesConditionalParameterizedQuery(int catNumber, bool expectsFilter)
+        [Fact]
+        public void GetProductsAndCategories_WithCatNumberGreaterThanZero_DoesNotThrowFromSqlConcatenation()
         {
             // Arrange
-            var provider = new MySqlDbProvider(new ConfigFile());
+            var nvc = new NameValueCollection
+            {
+                [DbConstants.KEY_HOST] = "localhost",
+                [DbConstants.KEY_PORT] = "3306",
+                [DbConstants.KEY_DATABASE] = "db",
+                [DbConstants.KEY_UID] = "u",
+                [DbConstants.KEY_PWD] = "",
+                [DbConstants.KEY_CLIENT_EXEC] = "mysql"
+            };
+            var provider = new MySqlDbProvider(new ConfigFile(nvc));
 
             // Act
-            var ex = Record.Exception(() => provider.GetProductsAndCategories(catNumber));
+            var ex = Record.Exception(() => provider.GetProductsAndCategories(1));
 
             // Assert
-            // We expect failures due to empty connection string, but not due to SQL concatenation of catNumber.
-            Assert.NotNull(ex);
-            Assert.DoesNotContain("catNumber = " + catNumber, ex.Message, StringComparison.OrdinalIgnoreCase);
-
-            // Also ensure method exists (guard against refactor removing conditional branches).
-            var method = typeof(MySqlDbProvider).GetMethod("GetProductsAndCategories", new[] { typeof(int) });
-            Assert.NotNull(method);
+            Assert.Null(ex);
         }
     }
 }

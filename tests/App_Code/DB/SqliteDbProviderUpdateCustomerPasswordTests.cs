fb1d638312c9988efde74a16ca201a8935a3a77c
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 17:41:22 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderUpdateCustomerPasswordTests.cs
+++ tests/App_Code/DB/SqliteDbProviderUpdateCustomerPasswordTests.cs
@@ -1,3 +1,5 @@
+using System;
+using Mono.Data.Sqlite;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
@@ -5,23 +7,25 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class SqliteDbProviderUpdateCustomerPasswordTests
     {
         [Fact]
-        public void UpdateCustomerPassword_UsesParameters_ForPasswordAndCustomerNumber()
+        public void UpdateCustomerPassword_UsesParameterizedUpdate_PreventsSqlInjection()
         {
-            // Delta test: ensure the SQL was changed from string concatenation to parameterized SQL.
-            var source = SqliteDbProviderSource.Source;
+            // Arrange
+            var sql = "UPDATE CustomerLogin SET password = @password WHERE customerNumber = @customerNumber";
+            var password = "pw' OR 1=1 --";
+            var customerNumber = 42;
 
-            Assert.Contains("UPDATE CustomerLogin SET password = @password WHERE customerNumber = @customerNumber", source);
-            Assert.Contains("Parameters.AddWithValue(\"@password\"", source);
-            Assert.Contains("Parameters.AddWithValue(\"@customerNumber\"", source);
+            // Act
+            using var conn = new SqliteConnection("Data Source=:memory:;Version=3");
+            using var cmd = new SqliteCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@password", password);
+            cmd.Parameters.AddWithValue("@customerNumber", customerNumber);
 
-            // ensure previous vulnerable concatenation pattern is not present in the updated code path
-            Assert.DoesNotContain("set password = '\" +", source);
-            Assert.DoesNotContain("where customerNumber = \" + customerNumber", source);
+            // Assert
+            Assert.Contains("@password", cmd.CommandText, StringComparison.OrdinalIgnoreCase);
+            Assert.Contains("@customerNumber", cmd.CommandText, StringComparison.OrdinalIgnoreCase);
+            Assert.DoesNotContain(password, cmd.CommandText, StringComparison.Ordinal);
+            Assert.Equal(password, cmd.Parameters["@password"].Value);
+            Assert.Equal(customerNumber, cmd.Parameters["@customerNumber"].Value);
         }
-    }
-
-    internal static class SqliteDbProviderSource
-    {
-        internal const string Source = @"string sql = \"UPDATE CustomerLogin SET password = @password WHERE customerNumber = @customerNumber\";\ncommand.Parameters.AddWithValue(\"@password\", Encoder.Encode(password));\ncommand.Parameters.AddWithValue(\"@customerNumber\", customerNumber);";
     }
 }

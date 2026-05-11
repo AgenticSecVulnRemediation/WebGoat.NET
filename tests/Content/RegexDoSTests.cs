b9e5468ddafc33f247b7caced240ee2b70f7e944
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 11:42:25 +0530

    Add delta unit test

--- tests/Content/RegexDoSTests.cs
+++ tests/Content/RegexDoSTests.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Threading;
-using OWASP.WebGoat.NET;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.Tests
@@ -8,23 +7,17 @@ namespace OWASP.WebGoat.NET.Tests
     public class RegexDoSTests
     {
         [Fact]
-        public void RegexDoS_RegexConstruction_WithUserPattern_CompletesWithinTimeoutBudget()
+        public void RegexConstruction_WithTimeout_ThrowsRegexMatchTimeoutException_ForCatastrophicBacktracking()
         {
-            // Arrange
-            // Delta scope: Regex is now constructed with a timeout to mitigate catastrophic backtracking.
-            // We indirectly validate by constructing a similar Regex and ensuring matching returns quickly.
-            var userSuppliedPattern = "^(a+)+$";
-            var input = new string('a', 5000) + "!";
+            // Delta security fix: Regex is now constructed with a timeout (1 second).
+            // This test demonstrates that catastrophic patterns do not hang indefinitely.
 
-            // Act
-            Exception? ex = Record.Exception(() =>
-            {
-                var re = new System.Text.RegularExpressions.Regex(userSuppliedPattern, System.Text.RegularExpressions.RegexOptions.None, TimeSpan.FromSeconds(1));
-                re.IsMatch(input);
-            });
+            var catastrophicPattern = "^(a+)+$";
+            var longInput = new string('a', 200_000) + "!";
 
-            // Assert
-            Assert.Null(ex);
+            var re = new Regex(catastrophicPattern, RegexOptions.None, TimeSpan.FromMilliseconds(50));
+
+            Assert.Throws<RegexMatchTimeoutException>(() => re.Match(longInput));
         }
     }
 }

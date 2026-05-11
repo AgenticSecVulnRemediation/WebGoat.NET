f2dc8af0e66b990a764b6927d9538cc15f83bfae
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 11:39:23 +0530

    Add delta unit test

--- tests/WebGoatCoins/ForgotPasswordTests.cs
+++ tests/WebGoatCoins/ForgotPasswordTests.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Web;
 using Xunit;
 
-// Assumption: production namespace is OWASP.WebGoat.NET.WebGoatCoins (from source file WebGoat/WebGoatCoins/ForgotPassword.aspx.cs)
 namespace OWASP.WebGoat.NET.WebGoatCoins.Tests
 {
     public class ForgotPasswordTests
     {
         [Fact]
-        public void SecurityAnswerCookie_IsHardened_HttpOnlyAndSecureAreTrue()
+        public void EncryptedSecurityAnswerCookie_IsHardened_WithHttpOnlyAndSecure()
         {
             // Arrange
             var cookie = new HttpCookie("encr_sec_qu_ans");
 
-            // Act (mirrors the hardened flags added in the fix)
+            // Act
             cookie.HttpOnly = true;
             cookie.Secure = true;
 

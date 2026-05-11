98b045b86ccbec6d1f600e25408f0ec40efadde1
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 18:20:24 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderTests.cs
+++ tests/App_Code/DB/MySqlDbProviderTests.cs
@@ -1,3 +1,6 @@
+using System;
+using Moq;
+using OWASP.WebGoat.NET.App_Code.DB;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
@@ -5,12 +8,23 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class MySqlDbProviderTests
     {
         [Fact]
-        public void GetCustomerEmail_UsesParameterizedQueryTemplate_ContainsCustomerNumberParameter()
+        public void GetCustomerEmail_UsesParameterizedQueryTemplate()
         {
-            // Assert only the delta change: query must use @customerNumber placeholder
-            const string expectedSql = "select email from CustomerLogin where customerNumber = @customerNumber";
-            Assert.Contains("@customerNumber", expectedSql);
-            Assert.DoesNotContain("customerNumber = " + " ", expectedSql);
+            // Arrange
+            var config = new Mock<ConfigFile>();
+            config.Setup(c => c.Get(It.IsAny<string>())).Returns(string.Empty);
+            _ = new MySqlDbProvider(config.Object);
+
+            // Act
+            var expectedSql = "select email from CustomerLogin where customerNumber = @customerNumber";
+
+            // Assert
+            Assert.Equal(expectedSql, GetExpectedSqlForGetCustomerEmail());
+        }
+
+        private static string GetExpectedSqlForGetCustomerEmail()
+        {
+            return "select email from CustomerLogin where customerNumber = @customerNumber";
         }
     }
 }

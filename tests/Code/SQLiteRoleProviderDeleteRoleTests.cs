a72e4702c7fd6ac73ca9177f0e2b1a5e859c9e4a
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-11 17:37:19 +0530

    Add delta unit test

--- tests/Code/SQLiteRoleProviderDeleteRoleTests.cs
+++ tests/Code/SQLiteRoleProviderDeleteRoleTests.cs
@@ -1,6 +1,5 @@
 using System;
 using Mono.Data.Sqlite;
-using TechInfoSystems.Data.SQLite;
 using Xunit;
 
 namespace TechInfoSystems.Data.SQLite.Tests
@@ -8,25 +7,18 @@ namespace TechInfoSystems.Data.SQLite.Tests
     public class SQLiteRoleProviderDeleteRoleTests
     {
         [Fact]
-        public void DeleteRole_UsesAtParameterName_DoesNotTreatRoleNameAsInlineSql()
+        public void DeleteRole_UsesAtParameter_ForRoleName_InSubquery()
         {
             // Arrange
-            // Regression: query now uses @RoleName instead of $RoleName for nested select.
-            // Validate that parameter can be added under the expected name.
+            using var cmd = new SqliteCommand();
 
-            using (var cn = new SqliteConnection("Data Source=:memory:;Version=3"))
-            using (var cmd = cn.CreateCommand())
-            {
-                cmd.CommandText = "SELECT 1 WHERE 1=@RoleName";
+            // Act
+            cmd.CommandText = "DELETE FROM [aspnet_UsersInRoles] WHERE (RoleId IN (SELECT RoleId FROM [aspnet_Roles] WHERE LoweredRoleName = @RoleName))";
+            cmd.Parameters.AddWithValue("@RoleName", "admin");
 
-                // Act
-                var ex = Record.Exception(() => cmd.Parameters.AddWithValue("@RoleName", "admin'); DROP TABLE x; --"));
-
-                // Assert
-                Assert.Null(ex);
-                Assert.Equal(1, cmd.Parameters.Count);
-                Assert.Equal("@RoleName", cmd.Parameters[0].ParameterName);
-            }
+            // Assert
+            Assert.Single(cmd.Parameters);
+            Assert.Contains("@RoleName", cmd.CommandText);
         }
     }
 }

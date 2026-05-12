c5696dc71b285e98e93ae36e0af99d5993a02147
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-12 14:23:58 +0530

    Add delta unit test

--- tests/App_Code/CookieManagerTests.cs
+++ tests/App_Code/CookieManagerTests.cs
@@ -9,14 +9,14 @@ namespace OWASP.WebGoat.NET.App_Code.Tests
     public class CookieManagerTests
     {
         [Fact]
-        public void SetCookie_WhenCalled_SetsHttpOnlyFlag()
+        public void SetCookie_WhenCalled_SetsHttpOnlyTrue()
         {
             // Arrange
             var ticket = new FormsAuthenticationTicket(
                 1,
                 "user",
                 DateTime.UtcNow,
-                DateTime.UtcNow.AddMinutes(5),
+                DateTime.UtcNow.AddMinutes(30),
                 false,
                 "data");
 
@@ -26,6 +26,26 @@ namespace OWASP.WebGoat.NET.App_Code.Tests
             // Assert
             Assert.NotNull(cookie);
             Assert.True(cookie.HttpOnly);
+        }
+
+        [Fact]
+        public void SetCookie_WhenTicketIsPersistent_SetsCookieExpiresToTicketExpiration()
+        {
+            // Arrange
+            DateTime expiration = DateTime.UtcNow.AddHours(1);
+            var ticket = new FormsAuthenticationTicket(
+                1,
+                "user",
+                DateTime.UtcNow,
+                expiration,
+                true,
+                "data");
+
+            // Act
+            HttpCookie cookie = CookieManager.SetCookie(ticket, "id", "value");
+
+            // Assert
+            Assert.Equal(expiration, cookie.Expires);
         }
     }
 }

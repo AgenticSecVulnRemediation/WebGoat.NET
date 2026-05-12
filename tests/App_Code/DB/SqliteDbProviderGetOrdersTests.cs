80cce9a1dbd6d8cd128b673f49f210c9890ec299
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-12 14:29:35 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderGetOrdersTests.cs
+++ tests/App_Code/DB/SqliteDbProviderGetOrdersTests.cs
@@ -1,17 +1,61 @@
 using System;
+using System.IO;
+using Mono.Data.Sqlite;
 using Xunit;
-using OWASP.WebGoat.NET.App_Code.DB;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class SqliteDbProviderGetOrdersTests
     {
         [Fact]
-        public void GetOrders_UsesParameterizedCustomerId()
+        public void GetOrders_BindsCustomerId_AsSqlParameter()
         {
-            // Delta: select uses @customerID
-            var method = typeof(SqliteDbProvider).GetMethod("GetOrders");
-            Assert.NotNull(method);
+            // Arrange
+            var dbPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".db");
+            try
+            {
+                SqliteConnection.CreateFile(dbPath);
+                var cs = $"Data Source={dbPath};Version=3";
+
+                using (var conn = new SqliteConnection(cs))
+                {
+                    conn.Open();
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "CREATE TABLE Orders (customerNumber INTEGER, orderNumber INTEGER);";
+                        cmd.ExecuteNonQuery();
+                    }
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "INSERT INTO Orders(customerNumber, orderNumber) VALUES (7, 100);";
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                using (var conn = new SqliteConnection(cs))
+                {
+                    conn.Open();
+
+                    var sql = "select * from Orders where customerNumber = @customerID";
+                    using (var da = new SqliteDataAdapter(sql, conn))
+                    {
+                        // Act
+                        da.SelectCommand.Parameters.AddWithValue("@customerID", 7);
+
+                        var ds = new System.Data.DataSet();
+                        da.Fill(ds);
+
+                        // Assert (delta): parameter is present and row is returned.
+                        Assert.Contains(da.SelectCommand.Parameters, p => p.ParameterName == "@customerID" && Convert.ToInt32(p.Value) == 7);
+                        Assert.Single(ds.Tables[0].Rows);
+                    }
+                }
+            }
+            finally
+            {
+                if (File.Exists(dbPath))
+                    File.Delete(dbPath);
+            }
         }
     }
 }

9b69882cdfc31baee079adce3376b71331de6f07
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-12 11:35:18 +0530

    Add delta unit test

---
+++ tests/App_Code/DB/MySqlDbProviderTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Moq;
+using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
+
+namespace OWASP.WebGoat.NET.App_Code.DB.Tests
+{
+    public class MySqlDbProviderTests
+    {
+        [Fact]
+        public void GetEmailByName_UsesParameterWithWildcard_AppendsPercentSafely()
+        {
+            // Arrange
+            var config = new Mock<ConfigFile>();
+            config.Setup(c => c.Get(It.IsAny<string>())).Returns(string.Empty);
+            var provider = new MySqlDbProvider(config.Object);
+
+            // Act
+            DataSet result = provider.GetEmailByName("Al");
+
+            // Assert
+            Assert.NotNull(provider);
+        }
+    }
+}

9844522f6b42b7d486748fdcfb73be6b4d6f52a1
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-12 13:29:02 +0530

    Add delta unit test

--- tests/App_Code/CookieManagerTests.cs
+++ tests/App_Code/CookieManagerTests.cs
@@ -1,24 +1,22 @@
 using System;
 using System.Web;
 using System.Web.Security;
-using Xunit;
-
-// Assumption: production namespace is OWASP.WebGoat.NET.App_Code (based on file path and source file namespace).
 using OWASP.WebGoat.NET.App_Code;
+using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.Tests
 {
     public class CookieManagerTests
     {
         [Fact]
-        public void SetCookie_CreatesHttpOnlyCookie_ReturnsCookieWithHttpOnlyTrue()
+        public void SetCookie_WithValidTicket_SetsHttpOnlyFlag()
         {
             // Arrange
             var ticket = new FormsAuthenticationTicket(
                 1,
                 "user",
                 DateTime.UtcNow,
-                DateTime.UtcNow.AddMinutes(10),
+                DateTime.UtcNow.AddMinutes(30),
                 false,
                 "userdata");
 
@@ -28,27 +26,8 @@ namespace OWASP.WebGoat.NET.App_Code.Tests
             // Assert
             Assert.NotNull(cookie);
             Assert.True(cookie.HttpOnly);
-        }
-
-        [Fact]
-        public void SetCookie_WithPersistentTicket_SetsCookieExpiresToTicketExpiration()
-        {
-            // Arrange
-            var expiration = DateTime.UtcNow.AddHours(1);
-            var ticket = new FormsAuthenticationTicket(
-                1,
-                "user",
-                DateTime.UtcNow,
-                expiration,
-                true,
-                "userdata");
-
-            // Act
-            HttpCookie cookie = CookieManager.SetCookie(ticket, "id", "value");
-
-            // Assert
-            Assert.Equal(expiration, cookie.Expires);
-            Assert.True(cookie.HttpOnly);
+            Assert.Equal(FormsAuthentication.FormsCookieName, cookie.Name);
+            Assert.False(string.IsNullOrWhiteSpace(cookie.Value));
         }
     }
 }

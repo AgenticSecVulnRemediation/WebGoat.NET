04d74674955a05807cf6761caf8782a6be3dbb0c
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 13:51:41 +0530

    Add delta unit test

--- tests/DefaultHttpOnlyCookieTests.cs
+++ tests/DefaultHttpOnlyCookieTests.cs
@@ -5,27 +5,24 @@ using Xunit;
 
 namespace OWASP.WebGoat.NET.Tests
 {
-    public class DefaultTests
+    public class DefaultHttpOnlyCookieTests
     {
         [Fact]
-        public void PageLoad_WhenDbConfigured_AddsServerCookieAsHttpOnly()
+        public void PageLoad_WhenDbConfigured_ServerCookieIsHttpOnly()
         {
-            // Arrange
-            // This is a delta test for the security fix: the "Server" cookie must be HttpOnly.
-            // We simulate HttpContext and invoke Page_Load via reflection.
+            // Delta: cookie.HttpOnly must be set to true.
+            // We validate via a controlled HttpContext and a minimal Page lifecycle invocation.
+
             var request = new HttpRequest("", "http://localhost/Default.aspx", "");
             var response = new HttpResponse(new System.IO.StringWriter());
             HttpContext.Current = new HttpContext(request, response);
 
             var page = new Default();
 
-            // Inject a fake Session via HttpContext
-            // (ASP.NET creates it lazily; this call ensures Session is available.)
-            var _ = HttpContext.Current.Session;
-
-            // Act
-            page.GetType().GetMethod("Page_Load", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
-                ?.Invoke(page, new object[] { page, EventArgs.Empty });
+            // Act: invoke Page_Load directly.
+            var mi = typeof(Default).GetMethod("Page_Load", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            Assert.NotNull(mi);
+            mi!.Invoke(page, new object[] { page, EventArgs.Empty });
 
             // Assert
             var cookie = HttpContext.Current.Response.Cookies["Server"];

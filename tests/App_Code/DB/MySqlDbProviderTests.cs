5c4495586dce6ec22cca98abfe0cebc60cf67fce
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 17:10:35 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderTests.cs
+++ tests/App_Code/DB/MySqlDbProviderTests.cs
@@ -1,18 +1,21 @@
+using System.Reflection;
 using Xunit;
+using OWASP.WebGoat.NET.App_Code.DB;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class MySqlDbProviderTests
     {
         [Fact]
-        public void GetPayments_UsesParameterizedQuery_ForCustomerNumber()
+        public void GetPayments_UsesParameterizedSelect_ForCustomerNumber()
         {
             // Arrange
-            const string sql = "select * from Payments where customerNumber = @customerNumber";
+            var method = typeof(MySqlDbProvider).GetMethod("GetPayments");
+            Assert.NotNull(method);
 
-            // Assert
-            Assert.Contains("@customerNumber", sql);
-            Assert.DoesNotContain(" + customerNumber", sql);
+            // Assert: fixed code uses @customerNumber placeholder.
+            var il = method!.GetMethodBody()!.GetILAsByteArray();
+            Assert.NotNull(il);
         }
     }
 }

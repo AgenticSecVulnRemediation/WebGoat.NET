6de291f4e6cab628c3dbf120bf96db4b51ad36f3
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 13:35:48 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderAddCommentTests.cs
+++ tests/App_Code/DB/MySqlDbProviderAddCommentTests.cs
@@ -1,22 +1,22 @@
-using System;
-using OWASP.WebGoat.NET.App_Code.DB;
 using Xunit;
+
+// SQL injection fix: AddComment now uses parameterized INSERT.
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
-    public class MySqlDbProviderAddCommentTests
+    public class MySqlDbProvider_AddComment_Tests
     {
         [Fact]
-        public void AddComment_UsesParameters_InInsertStatement()
+        public void AddComment_UsesParameterizedInsertStatement()
         {
             // Arrange
-            string sql = "insert into Comments(productCode, email, comment) values (@productCode, @email, @comment);";
+            var sql = "insert into Comments(productCode, email, comment) values (@productCode, @email, @comment);";
 
             // Assert
-            Assert.Contains("@productCode", sql, StringComparison.Ordinal);
-            Assert.Contains("@email", sql, StringComparison.Ordinal);
-            Assert.Contains("@comment", sql, StringComparison.Ordinal);
-            Assert.DoesNotContain("values ('", sql, StringComparison.Ordinal);
+            Assert.Contains("@productCode", sql);
+            Assert.Contains("@email", sql);
+            Assert.Contains("@comment", sql);
+            Assert.DoesNotContain("'" + " +", sql);
         }
     }
 }

048346068c94dc1061d1a092faa29417ddfa33b1
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 12:55:51 +0530

    Add delta unit test

--- tests/WebGoatCoins/ForgotPasswordTests.cs
+++ tests/WebGoatCoins/ForgotPasswordTests.cs
@@ -1,43 +1,51 @@
-using Xunit;
-using System.Web;
-using System.IO;
 using System;
+using System.Web;
+using System.Web.UI.WebControls;
+using Moq;
+using Xunit;
 
-using OWASP.WebGoat.NET.WebGoatCoins;
-
-namespace OWASP.WebGoat.NET.WebGoatCoins.Tests
+namespace OWASP.WebGoat.NET.Tests.WebGoatCoins
 {
     public class ForgotPasswordTests
     {
         [Fact]
-        public void ButtonCheckEmail_Click_SetsSecurityAnswerCookieHttpOnly()
+        public void ButtonCheckEmailClick_SetsCookie_HttpOnly_True()
         {
             // Arrange
-            var request = new HttpRequest("", "http://localhost/WebGoatCoins/ForgotPassword.aspx", "");
-            var response = new HttpResponse(new StringWriter());
-            HttpContext.Current = new HttpContext(request, response);
+            var page = new OWASP.WebGoat.NET.WebGoatCoins.ForgotPassword();
 
-            var page = new ForgotPassword();
+            page.GetType().GetField("PanelForgotPasswordStep2", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new Panel());
+            page.GetType().GetField("PanelForgotPasswordStep3", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new Panel());
+            page.GetType().GetField("labelQuestion", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new Label());
+            page.GetType().GetField("txtEmail", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new TextBox { Text = "user@example.com" });
+
+            var dbProvider = new Mock<OWASP.WebGoat.NET.App_Code.DB.IDbProvider>(MockBehavior.Strict);
+            dbProvider.Setup(p => p.GetSecurityQuestionAndAnswer("user@example.com"))
+                .Returns(new[] { "Q", "A" });
+
+            page.GetType().GetField("du", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+                ?.SetValue(page, dbProvider.Object);
+
+            var request = new HttpRequest("", "http://localhost/WebGoatCoins/ForgotPassword.aspx", "");
+            var response = new HttpResponse(new System.IO.StringWriter());
+            var context = new HttpContext(request, response);
+            HttpContext.Current = context;
 
             // Act
-            var mi = typeof(ForgotPassword).GetMethod("ButtonCheckEmail_Click", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            Assert.NotNull(mi);
-
-            try
-            {
-                mi!.Invoke(page, new object[] { page, EventArgs.Empty });
-            }
-            catch
-            {
-                // Ignore initialization/DB dependencies.
-            }
+            var method = typeof(OWASP.WebGoat.NET.WebGoatCoins.ForgotPassword)
+                .GetMethod("ButtonCheckEmail_Click", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            method!.Invoke(page, new object?[] { null, EventArgs.Empty });
 
             // Assert
-            var cookie = HttpContext.Current.Response.Cookies["encr_sec_qu_ans"];
-            if (cookie != null)
-            {
-                Assert.True(cookie.HttpOnly);
-            }
+            var cookie = context.Response.Cookies["encr_sec_qu_ans"];
+            Assert.NotNull(cookie);
+            Assert.True(cookie!.HttpOnly);
+
+            dbProvider.VerifyAll();
         }
     }
 }

33903db5f3fabcf039a9704fd48a31d29804e701
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 12:56:08 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetOrdersTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetOrdersTests.cs
@@ -1,31 +1,18 @@
 using Xunit;
-using Moq;
-using MySql.Data.MySqlClient;
-using System.Data;
 
-using OWASP.WebGoat.NET.App_Code.DB;
-using OWASP.WebGoat.NET.App_Code;
-
-namespace OWASP.WebGoat.NET.App_Code.DB.Tests
+namespace OWASP.WebGoat.NET.Tests.App_Code.DB
 {
     public class MySqlDbProviderGetOrdersTests
     {
         [Fact]
-        public void GetOrders_UsesParameterizedQuery_ForCustomerNumber()
+        public void GetOrders_UsesParameterizedCustomerId()
         {
-            // This is a regression test for the security fix: query now uses @customerID parameter.
-            // We can't intercept MySqlDataAdapter internals without integration; instead we assert
-            // the expected SQL text and that parameter is added as shown in the diff.
-
-            const string expectedSql = "select * from Orders where customerNumber = @customerID";
-
             // Arrange
-            var cmd = new MySqlCommand(expectedSql);
-            cmd.Parameters.AddWithValue("@customerID", 1);
+            const string sql = "select * from Orders where customerNumber = @customerID";
 
             // Assert
-            Assert.Equal(expectedSql, cmd.CommandText);
-            Assert.True(cmd.Parameters.Contains("@customerID"));
+            Assert.Contains("@customerID", sql);
+            Assert.DoesNotContain("customerNumber = \" + customerID", sql);
         }
     }
 }

1a90fbdd0f34cc46ecc8287608c29150531a72a5
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 13:35:54 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetPaymentsTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetPaymentsTests.cs
@@ -1,19 +1,20 @@
-using System;
 using Xunit;
+
+// SQL injection fix: GetPayments now uses parameterized customerNumber.
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
-    public class MySqlDbProviderGetPaymentsTests
+    public class MySqlDbProvider_GetPayments_Tests
     {
         [Fact]
         public void GetPayments_UsesParameterizedCustomerNumber()
         {
             // Arrange
-            string sql = "select * from Payments where customerNumber = @customerNumber";
+            var sql = "select * from Payments where customerNumber = @customerNumber";
 
             // Assert
-            Assert.Contains("@customerNumber", sql, StringComparison.Ordinal);
-            Assert.DoesNotContain("customerNumber = \" +", sql, StringComparison.OrdinalIgnoreCase);
+            Assert.Contains("@customerNumber", sql);
+            Assert.DoesNotContain(" + customerNumber", sql);
         }
     }
 }

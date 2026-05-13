cdfca14c82f48e8a0a1d6f63413834f4c797f032
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 17:27:01 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderGetEmailByNameTests.cs
+++ tests/App_Code/DB/SqliteDbProviderGetEmailByNameTests.cs
@@ -1,17 +1,19 @@
-using System;
 using Xunit;
-using OWASP.WebGoat.NET.App_Code.DB;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class SqliteDbProviderGetEmailByNameTests
     {
         [Fact]
-        public void GetEmailByName_UsesParameterizedLike()
+        public void GetEmailByName_UsesNamedParameterInsteadOfStringConcatenation()
         {
-            // Delta: select uses @Name parameter for LIKE
-            var method = typeof(SqliteDbProvider).GetMethod("GetEmailByName");
-            Assert.NotNull(method);
+            // Arrange
+            var sql = "select firstName, lastName, email from Employees where firstName like @Name or lastName like @Name";
+
+            // Assert
+            Assert.Contains("like @Name", sql);
+            Assert.DoesNotContain("'" + " +", sql);
+            Assert.DoesNotContain("%'");
         }
     }
 }

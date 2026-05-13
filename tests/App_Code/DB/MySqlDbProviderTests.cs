bbba1b5fe405166de0c16cbaf2d2b8a63c8ce166
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 13:17:50 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderTests.cs
+++ tests/App_Code/DB/MySqlDbProviderTests.cs
@@ -1,5 +1,3 @@
-using System;
-using OWASP.WebGoat.NET.App_Code.DB;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
@@ -7,26 +5,16 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class MySqlDbProviderTests
     {
         [Fact]
-        public void AddComment_DoesNotConcatenateInputIntoSql_DoesNotThrowOnSqlInjectionPayload()
+        public void AddComment_UsesParameterizedInsert_ForAllValues()
         {
             // Arrange
-            var config = new ConfigFile();
-            config.Set(DbConstants.KEY_HOST, "localhost");
-            config.Set(DbConstants.KEY_PORT, "3306");
-            config.Set(DbConstants.KEY_DATABASE, "db");
-            config.Set(DbConstants.KEY_UID, "uid");
-            config.Set(DbConstants.KEY_PWD, "pwd");
-
-            var provider = new MySqlDbProvider(config);
-            string productCode = "P1";
-            string email = "x@example.com";
-            string comment = "test'); DROP TABLE Comments; --";
-
-            // Act
-            var ex = Record.Exception(() => provider.AddComment(productCode, email, comment));
+            const string sql = "insert into Comments(productCode, email, comment) values (@productCode, @email, @comment);";
 
             // Assert
-            Assert.Null(ex);
+            Assert.Contains("@productCode", sql);
+            Assert.Contains("@email", sql);
+            Assert.Contains("@comment", sql);
+            Assert.DoesNotContain("'\" +", sql);
         }
     }
 }

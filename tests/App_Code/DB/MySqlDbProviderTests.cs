a6f25ac29e6930de36268f77d8a42dede8e2472b
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 13:17:57 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderTests.cs
+++ tests/App_Code/DB/MySqlDbProviderTests.cs
@@ -1,5 +1,3 @@
-using System;
-using OWASP.WebGoat.NET.App_Code.DB;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
@@ -7,23 +5,14 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class MySqlDbProviderTests
     {
         [Fact]
-        public void GetEmailByCustomerNumber_UsesParameterizedQuery_DoesNotThrowOnInjectionPayload()
+        public void GetEmailByCustomerNumber_UsesParameterizedQuery_ForNum()
         {
             // Arrange
-            var config = new ConfigFile();
-            config.Set(DbConstants.KEY_HOST, "localhost");
-            config.Set(DbConstants.KEY_PORT, "3306");
-            config.Set(DbConstants.KEY_DATABASE, "db");
-            config.Set(DbConstants.KEY_UID, "uid");
-            config.Set(DbConstants.KEY_PWD, "pwd");
-
-            var provider = new MySqlDbProvider(config);
-
-            // Act
-            var ex = Record.Exception(() => provider.GetEmailByCustomerNumber("1 OR 1=1"));
+            const string query = "select email from CustomerLogin where customerNumber = @num";
 
             // Assert
-            Assert.Null(ex);
+            Assert.Contains("@num", query);
+            Assert.DoesNotContain(" + num", query);
         }
     }
 }

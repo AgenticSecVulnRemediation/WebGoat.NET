630d1ff72389b177f6766a57416b6bdfa995b8a4
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 13:36:02 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderGetPaymentsTests.cs
+++ tests/App_Code/DB/SqliteDbProviderGetPaymentsTests.cs
@@ -1,19 +1,20 @@
-using System;
 using Xunit;
+
+// SQL injection fix: SqliteDbProvider.GetPayments now uses parameterized customerNumber.
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
-    public class SqliteDbProviderGetPaymentsTests
+    public class SqliteDbProvider_GetPayments_Tests
     {
         [Fact]
         public void GetPayments_UsesParameterizedCustomerNumber()
         {
             // Arrange
-            string sql = "select * from Payments where customerNumber = @customerNumber";
+            var sql = "select * from Payments where customerNumber = @customerNumber";
 
             // Assert
-            Assert.Contains("@customerNumber", sql, StringComparison.Ordinal);
-            Assert.DoesNotContain("customerNumber = \" + customerNumber", sql, StringComparison.OrdinalIgnoreCase);
+            Assert.Contains("@customerNumber", sql);
+            Assert.DoesNotContain(" + customerNumber", sql);
         }
     }
 }

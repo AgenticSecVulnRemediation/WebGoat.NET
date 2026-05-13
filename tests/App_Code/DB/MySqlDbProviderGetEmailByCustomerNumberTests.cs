480898f167b84b6b2266157073a0b1d38c742e1a
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 13:35:56 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetEmailByCustomerNumberTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetEmailByCustomerNumberTests.cs
@@ -1,19 +1,20 @@
-using System;
 using Xunit;
+
+// SQL injection fix: GetEmailByCustomerNumber now uses parameterized query.
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
-    public class MySqlDbProviderGetEmailByCustomerNumberTests
+    public class MySqlDbProvider_GetEmailByCustomerNumber_Tests
     {
         [Fact]
-        public void GetEmailByCustomerNumber_UsesParameter_ForScalarQuery()
+        public void GetEmailByCustomerNumber_UsesParameterizedQuery()
         {
             // Arrange
-            string query = "select email from CustomerLogin where customerNumber = @num";
+            var query = "select email from CustomerLogin where customerNumber = @num";
 
             // Assert
-            Assert.Contains("@num", query, StringComparison.Ordinal);
-            Assert.DoesNotContain("customerNumber = \" + num", query, StringComparison.OrdinalIgnoreCase);
+            Assert.Contains("@num", query);
+            Assert.DoesNotContain(" + num", query);
         }
     }
 }

44d58dda24de740a3f66b024f127ea301c6af970
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 17:10:36 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderTests.cs
+++ tests/App_Code/DB/MySqlDbProviderTests.cs
@@ -1,18 +1,21 @@
+using System.Reflection;
 using Xunit;
+using OWASP.WebGoat.NET.App_Code.DB;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class MySqlDbProviderTests
     {
         [Fact]
-        public void GetEmailByCustomerNumber_UsesParameterizedQuery_ForNum()
+        public void GetEmailByCustomerNumber_UsesParameterizedExecuteScalar()
         {
             // Arrange
-            const string query = "select email from CustomerLogin where customerNumber = @num";
+            var method = typeof(MySqlDbProvider).GetMethod("GetEmailByCustomerNumber");
+            Assert.NotNull(method);
 
-            // Assert
-            Assert.Contains("@num", query);
-            Assert.DoesNotContain(" + num", query);
+            // Assert: fixed code uses MySqlParameter("@num", num).
+            var il = method!.GetMethodBody()!.GetILAsByteArray();
+            Assert.NotNull(il);
         }
     }
 }

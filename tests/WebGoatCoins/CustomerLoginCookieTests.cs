3751d159c193f724fa67615b29798b98cea8a6f7
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-13 12:55:53 +0530

    Add delta unit test

--- tests/WebGoatCoins/CustomerLoginCookieTests.cs
+++ tests/WebGoatCoins/CustomerLoginCookieTests.cs
@@ -1,52 +1,59 @@
-using Xunit;
-using System.Web;
-using System.IO;
 using System;
+using System.Web;
+using System.Web.UI.WebControls;
+using Moq;
+using Xunit;
 
-// Assumption: page class is OWASP.WebGoat.NET.WebGoatCoins.CustomerLogin
-using OWASP.WebGoat.NET.WebGoatCoins;
-
-namespace OWASP.WebGoat.NET.WebGoatCoins.Tests
+namespace OWASP.WebGoat.NET.Tests.WebGoatCoins
 {
     public class CustomerLoginCookieTests
     {
         [Fact]
-        public void ButtonLogOnClick_SetsAuthCookieHttpOnly()
+        public void ButtonLogOnClick_SetsAuthCookie_HttpOnly_True()
         {
-            // Arrange: minimal HttpContext to allow Response.Cookies.Add
-            var request = new HttpRequest("", "http://localhost/WebGoatCoins/CustomerLogin.aspx", "");
-            var response = new HttpResponse(new StringWriter());
-            HttpContext.Current = new HttpContext(request, response);
+            // Arrange
+            var page = new OWASP.WebGoat.NET.WebGoatCoins.CustomerLogin();
 
-            var page = new CustomerLogin();
+            page.GetType().GetField("PanelError", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new Panel());
+            page.GetType().GetField("labelError", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new Label());
+            page.GetType().GetField("txtUserName", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new TextBox { Text = "user@example.com" });
+            page.GetType().GetField("txtPassword", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)
+                ?.SetValue(page, new TextBox { Text = "pw" });
+
+            var dbProvider = new Mock<OWASP.WebGoat.NET.App_Code.DB.IDbProvider>(MockBehavior.Strict);
+            dbProvider.Setup(p => p.IsValidCustomerLogin("user@example.com", "pw")).Returns(true);
+            page.GetType().GetField("du", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+                ?.SetValue(page, dbProvider.Object);
+
+            var request = new HttpRequest("", "http://localhost/WebGoatCoins/CustomerLogin.aspx", "");
+            var response = new HttpResponse(new System.IO.StringWriter());
+            var context = new HttpContext(request, response);
+            HttpContext.Current = context;
 
             // Act
-            var mi = typeof(CustomerLogin).GetMethod("ButtonLogOn_Click", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            Assert.NotNull(mi);
+            var method = typeof(OWASP.WebGoat.NET.WebGoatCoins.CustomerLogin)
+                .GetMethod("ButtonLogOn_Click", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
 
+            // The handler calls Response.Redirect; intercept via HttpResponse.RedirectLocation after catching ThreadAbortException in ASP.NET,
+            // but under this harness it may throw. We only need cookie behavior, so swallow exceptions.
             try
             {
-                mi!.Invoke(page, new object[] { page, EventArgs.Empty });
+                method!.Invoke(page, new object?[] { null, EventArgs.Empty });
             }
             catch
             {
-                // Ignore: depends on controls and DB provider; we validate emitted auth cookie is hardened if present.
+                // ignore
             }
 
             // Assert
-            // Default FormsAuthentication cookie name is ".ASPXAUTH" but framework may vary; we check any cookie with HttpOnly set.
-            bool anyHttpOnly = false;
-            foreach (string key in HttpContext.Current.Response.Cookies)
-            {
-                var c = HttpContext.Current.Response.Cookies[key];
-                if (c != null && c.HttpOnly)
-                {
-                    anyHttpOnly = true;
-                    break;
-                }
-            }
+            var authCookie = context.Response.Cookies[System.Web.Security.FormsAuthentication.FormsCookieName];
+            Assert.NotNull(authCookie);
+            Assert.True(authCookie!.HttpOnly);
 
-            Assert.True(anyHttpOnly);
+            dbProvider.VerifyAll();
         }
     }
 }

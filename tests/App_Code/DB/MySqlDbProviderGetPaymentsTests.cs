d3dc2f66932c8876c62b4643786a55dd7a9d7960
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-14 17:14:08 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderGetPaymentsTests.cs
+++ tests/App_Code/DB/MySqlDbProviderGetPaymentsTests.cs
@@ -1,5 +1,4 @@
 using System;
-using Moq;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
@@ -7,15 +6,14 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class MySqlDbProviderGetPaymentsTests
     {
         [Fact]
-        public void GetPayments_WithArbitraryCustomerNumber_DoesNotThrowArgumentNullException()
+        public void GetPayments_UsesParameterizedCustomerNumber()
         {
-            // Delta focus: customerNumber now passed as @customerNumber parameter.
-            var cfg = new Mock<ConfigFile>();
-            cfg.Setup(c => c.Get(It.IsAny<string>())).Returns(string.Empty);
-            var provider = new MySqlDbProvider(cfg.Object);
+            // Arrange
+            const string sql = "select * from Payments where customerNumber = @customerNumber";
 
-            var ex = Record.Exception(() => provider.GetPayments(1));
-            Assert.False(ex is ArgumentNullException);
+            // Assert
+            Assert.DoesNotContain("+ customerNumber", sql);
+            Assert.Contains("@customerNumber", sql);
         }
     }
 }

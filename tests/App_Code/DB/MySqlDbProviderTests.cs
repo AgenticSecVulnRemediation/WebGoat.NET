05c126c0340b3c187055e0f8a69c82f8ca7fdd4e
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-14 20:35:45 +0530

    Add delta unit test

--- tests/App_Code/DB/MySqlDbProviderTests.cs
+++ tests/App_Code/DB/MySqlDbProviderTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Moq;
 using OWASP.WebGoat.NET.App_Code.DB;
 using Xunit;
 
@@ -7,15 +8,22 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class MySqlDbProviderTests
     {
         [Fact]
-        public void GetPasswordByEmail_UsesParameterizedQuery()
+        public void GetPasswordByEmail_AllowsEmailWithQuotes_WithoutThrowing_FromSqlConcatenation()
         {
+            // Delta test for fix: SQL concatenation -> parameterized select.
+
             // Arrange
-            const string expectedSql = "select * from CustomerLogin where email = @email;";
-            var injection = "x' OR 1=1 --";
+            var config = new Mock<ConfigFile>();
+            config.Setup(c => c.Get(It.IsAny<string>())).Returns(string.Empty);
+            var provider = new MySqlDbProvider(config.Object);
+
+            string email = "a' OR '1'='1";
+
+            // Act
+            var ex = Record.Exception(() => provider.GetPasswordByEmail(email));
 
             // Assert
-            Assert.Contains("@email", expectedSql);
-            Assert.DoesNotContain(injection, expectedSql);
+            Assert.Null(ex);
         }
     }
 }

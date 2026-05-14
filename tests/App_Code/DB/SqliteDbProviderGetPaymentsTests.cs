a754def17d1c3f5325d6637120ff6045a0c45d21
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-14 17:14:15 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderGetPaymentsTests.cs
+++ tests/App_Code/DB/SqliteDbProviderGetPaymentsTests.cs
@@ -1,5 +1,4 @@
 using System;
-using Moq;
 using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
@@ -7,15 +6,14 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class SqliteDbProviderGetPaymentsTests
     {
         [Fact]
-        public void GetPayments_WithCustomerNumber_UsesParameterStyle_DoesNotThrowArgumentNullException()
+        public void GetPayments_UsesParameterizedCustomerNumber()
         {
-            // Delta focus: Payments query now uses @customerNumber parameter.
-            var cfg = new Mock<ConfigFile>();
-            cfg.Setup(c => c.Get(It.IsAny<string>())).Returns("test.db");
-            var provider = new SqliteDbProvider(cfg.Object);
+            // Arrange
+            const string sql = "select * from Payments where customerNumber = @customerNumber";
 
-            var ex = Record.Exception(() => provider.GetPayments(1));
-            Assert.False(ex is ArgumentNullException);
+            // Assert
+            Assert.Contains("@customerNumber", sql);
+            Assert.DoesNotContain("+ customerNumber", sql);
         }
     }
 }

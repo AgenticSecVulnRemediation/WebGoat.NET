d5b697403eb79ebc27aef509b42a17ae9e8ee17d
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-14 20:16:20 +0530

    Add delta unit test

--- tests/WebGoatCoins/CustomerLoginLoggingTests.cs
+++ tests/WebGoatCoins/CustomerLoginLoggingTests.cs
@@ -6,10 +6,10 @@ namespace OWASP.WebGoat.NET.WebGoatCoins.Tests
     public class CustomerLoginLoggingTests
     {
         [Fact]
-        public void CustomerLogin_DoesNotLogPasswordInMessage()
+        public void ButtonLogOnClick_DoesNotLogPassword_Smoke()
         {
-            // Delta behavior: log message should no longer include password.
-            // Requires log4net inspection; here we enforce class presence.
+            // Delta behavior: log message removed password.
+            // Smoke test that page type is present; log verification would require injectable logger.
             Assert.NotNull(typeof(CustomerLogin));
         }
     }

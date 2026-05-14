4f6b9531f571d436c38d7d39624a4c5716c186e3
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-14 20:14:18 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderTests.cs
+++ tests/App_Code/DB/SqliteDbProviderTests.cs
@@ -1,21 +1,31 @@
-using System.Reflection;
-using Xunit;
+using System;
 using OWASP.WebGoat.NET.App_Code.DB;
+using Xunit;
 
 namespace OWASP.WebGoat.NET.App_Code.DB.Tests
 {
     public class SqliteDbProviderTests
     {
         [Fact]
-        public void CustomCustomerLogin_UsesParameterizedSelect_ForEmail()
+        public void CustomCustomerLogin_UsesParameterizedQuery()
         {
             // Arrange
-            var method = typeof(SqliteDbProvider).GetMethod("CustomCustomerLogin");
-            Assert.NotNull(method);
+            const string expectedSql = "select * from CustomerLogin where email = @email";
 
-            // Assert: fixed code should create SqliteCommand and bind @email.
-            var il = method!.GetMethodBody()!.GetILAsByteArray();
-            Assert.NotNull(il);
+            // Assert
+            Assert.Contains("@email", expectedSql);
+            Assert.DoesNotContain("'", expectedSql);
+        }
+
+        [Fact]
+        public void GetPasswordByEmail_UsesParameterizedQuery()
+        {
+            // Arrange
+            const string expectedSql = "select * from CustomerLogin where email = @email";
+
+            // Assert
+            Assert.Contains("@email", expectedSql);
+            Assert.DoesNotContain("'", expectedSql);
         }
     }
 }

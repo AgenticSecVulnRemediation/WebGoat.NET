36179ed39dcbab8425e3cfe7dad2fdf56d545eb1
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-14 20:35:52 +0530

    Add delta unit test

--- tests/App_Code/DB/SqliteDbProviderTests.cs
+++ tests/App_Code/DB/SqliteDbProviderTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Moq;
 using OWASP.WebGoat.NET.App_Code.DB;
 using Xunit;
 
@@ -7,16 +8,24 @@ namespace OWASP.WebGoat.NET.App_Code.DB.Tests
     public class SqliteDbProviderTests
     {
         [Fact]
-        public void IsValidCustomerLogin_ParameterizedQuery_PreventsInlineEmailAndPassword()
+        public void IsValidCustomerLogin_AcceptsQuoteCharacters_WithoutThrowing_FromSqlConcatenation()
         {
+            // Delta test for fix: SQL concatenation -> parameterized query.
+            // We assert the method doesn't throw when given quote-containing inputs.
+
             // Arrange
-            const string expectedSql = "select * from CustomerLogin where email = @email and password = @password;";
-            var injection = "x' OR 1=1 --";
+            var config = new Mock<ConfigFile>();
+            config.Setup(c => c.Get(It.IsAny<string>())).Returns(string.Empty);
+            var provider = new SqliteDbProvider(config.Object);
+
+            string email = "a' OR '1'='1";
+            string password = "p' OR '1'='1";
+
+            // Act
+            var ex = Record.Exception(() => provider.IsValidCustomerLogin(email, password));
 
             // Assert
-            Assert.Contains("@email", expectedSql);
-            Assert.Contains("@password", expectedSql);
-            Assert.DoesNotContain(injection, expectedSql);
+            Assert.Null(ex);
         }
     }
 }

29205c93f97e8d8d8071872390270b1831675581
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-14 20:37:53 +0530

    Add delta unit test

--- tests/Content/RegexDoSTests.cs
+++ tests/Content/RegexDoSTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using Xunit;
 
@@ -7,13 +8,18 @@ namespace OWASP.WebGoat.NET.Tests
     public class RegexDoSTests
     {
         [Fact]
-        public void RegexConstructor_UsesTimeout_ToMitigateCatastrophicBacktracking()
+        public void RegexConstruction_WithUserControlledPattern_EnforcesTimeout()
         {
-            // Delta behavior: regex created with explicit timeout.
-            // If a pathological pattern is used, a timeout should throw RegexMatchTimeoutException.
-            var regex = new Regex("^(a+)+$", RegexOptions.None, TimeSpan.FromMilliseconds(1));
+            // Arrange
+            // Delta: Regex is now created with a 1000ms timeout.
+            // Use catastrophic backtracking input; without timeout, could run excessively.
+            string evilPattern = "^(a+)+$";
+            string input = new string('a', 20000) + "!";
 
-            Assert.Throws<RegexMatchTimeoutException>(() => regex.Match(new string('a', 10000) + "!"));
+            var regex = new Regex(evilPattern, RegexOptions.None, TimeSpan.FromMilliseconds(1000));
+
+            // Act + Assert
+            Assert.Throws<RegexMatchTimeoutException>(() => regex.Match(input));
         }
     }
 }

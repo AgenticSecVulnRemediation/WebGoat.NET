2c19ceb1ee006e888d3655d6f66a20710725e1e7
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-15 16:50:35 +0530

    Add delta unit test

---
+++ tests/App_Code/DB/MySqlDbProviderGetOrdersParameterizedTests.cs
@@ -0,0 +1,31 @@
+using System;
+using MySql.Data.MySqlClient;
+using Xunit;
+
+using OWASP.WebGoat.NET.App_Code.DB;
+
+namespace OWASP.WebGoat.NET.App_Code.DB.Tests
+{
+    public class MySqlDbProviderGetOrdersParameterizedTests
+    {
+        [Fact]
+        public void GetOrders_WithInjectionPayload_DoesNotThrowSqlSyntaxErrorFromConcatenation()
+        {
+            // Delta intent: customerID now parameterized.
+            var cfg = new ConfigFile();
+            cfg.Set(DbConstants.KEY_HOST, "localhost");
+            cfg.Set(DbConstants.KEY_PORT, "3306");
+            cfg.Set(DbConstants.KEY_DATABASE, "db");
+            cfg.Set(DbConstants.KEY_UID, "user");
+            cfg.Set(DbConstants.KEY_PWD, "pwd");
+
+            var provider = new MySqlDbProvider(cfg);
+
+            var ex = Record.Exception(() => provider.GetOrders(1));
+            if (ex is MySqlException mysqlEx)
+            {
+                Assert.DoesNotContain("You have an error in your SQL syntax", mysqlEx.Message, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    }
+}

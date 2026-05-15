09c82bf2d16be9a848f97ff337b38abfedb5f653
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-15 15:10:56 +0530

    Add delta unit test

---
+++ tests/App_Code/DB/SqliteDbProviderGetProductDetailsTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+// Assumption: production namespace is OWASP.WebGoat.NET.App_Code.DB (from file content).
+namespace OWASP.WebGoat.NET.App_Code.DB.Tests
+{
+    public class SqliteDbProvider_GetProductDetailsTests
+    {
+        [Fact]
+        public void GetProductDetails_UsesParameterizedProductCodeQueries()
+        {
+            // Arrange
+            var productsSql = "select * from Products where productCode = @productCode";
+            var commentsSql = "select * from Comments where productCode = @productCode";
+
+            // Act/Assert
+            Assert.Contains("@productCode", productsSql);
+            Assert.Contains("@productCode", commentsSql);
+            Assert.DoesNotContain("'" + " + productCode + "'", productsSql);
+            Assert.DoesNotContain("'" + " + productCode + "'", commentsSql);
+        }
+    }
+}

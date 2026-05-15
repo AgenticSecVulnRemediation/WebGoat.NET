8ac60ba040ae829d8f893cd2aebf03ad4ba42882
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-15 19:00:51 +0530

    Add delta unit test

---
+++ tests/App_Code/CookieManagerTests.cs
@@ -0,0 +1,35 @@
+// Assumption: project uses xUnit and compiles test project with access to System.Web
+// Tests only validate the behavior added in PR diff: HttpOnly and Secure flags are set on auth cookie.
+
+using System;
+using System.Web;
+using System.Web.Security;
+using Xunit;
+using OWASP.WebGoat.NET.App_Code;
+
+namespace OWASP.WebGoat.NET.App_Code.Tests
+{
+    public class CookieManagerTests
+    {
+        [Fact]
+        public void SetCookie_SetsHttpOnlyAndSecureFlags_ReturnsCookieWithFlagsEnabled()
+        {
+            // Arrange
+            var ticket = new FormsAuthenticationTicket(
+                1,
+                "user",
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(30),
+                false,
+                "data");
+
+            // Act
+            HttpCookie cookie = CookieManager.SetCookie(ticket, "id", "value");
+
+            // Assert
+            Assert.NotNull(cookie);
+            Assert.True(cookie.HttpOnly);
+            Assert.True(cookie.Secure);
+        }
+    }
+}

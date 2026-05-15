968e2869ee6b9c8be347f26fa97724717ad3b3a0
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-15 17:02:16 +0530

    Add delta unit test

--- tests/Content/RegexDoSTests.cs
+++ tests/Content/RegexDoSTests.cs
@@ -1,28 +1,21 @@
 using System;
+using System.Text.RegularExpressions;
 using Xunit;
-
-// Assumption: page class is OWASP.WebGoat.NET.RegexDoS in WebGoat/Content/RegexDoS.aspx.cs
-using OWASP.WebGoat.NET;
 
 namespace OWASP.WebGoat.NET.Tests
 {
     public class RegexDoSTests
     {
         [Fact]
-        public void RegexDoS_UsesTimeout_ToPreventReDoS()
+        public void RegexCtor_WithTimeout_ThrowsRegexMatchTimeoutException_OnCatastrophicBacktracking()
         {
             // Arrange
-            var page = new RegexDoS();
+            var userName = "(a+)+$";
+            var password = new string('a', 20000);
+            var re = new Regex(userName, RegexOptions.None, TimeSpan.FromSeconds(1));
 
             // Act + Assert
-            // The vulnerability fix is the explicit timeout passed to Regex constructor.
-            // We can't access the local Regex instance directly; instead verify that a pathological pattern triggers a RegexMatchTimeoutException.
-            var userName = "(a+)+$";
-            var password = new string('a', 10000);
-
-            // Mimic the same regex construction as in btnCreate_Click.
-            var re = new System.Text.RegularExpressions.Regex(userName, System.Text.RegularExpressions.RegexOptions.None, TimeSpan.FromSeconds(1));
-            Assert.Throws<System.Text.RegularExpressions.RegexMatchTimeoutException>(() => re.Match(password));
+            Assert.Throws<RegexMatchTimeoutException>(() => re.Match(password));
         }
     }
 }

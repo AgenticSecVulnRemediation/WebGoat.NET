8e09ff2ba5431ed5974c96958d99fa74d2afba0f
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-15 17:02:21 +0530

    Add delta unit test

--- tests/WebGoatCoins/ForgotPasswordTests.cs
+++ tests/WebGoatCoins/ForgotPasswordTests.cs
@@ -3,11 +3,9 @@ using System.Web;
 using System.Web.UI.WebControls;
 using Moq;
 using Xunit;
-
-// Assumption: page class is OWASP.WebGoat.NET.WebGoatCoins.ForgotPassword
-using OWASP.WebGoat.NET.WebGoatCoins;
 using OWASP.WebGoat.NET.App_Code;
 using OWASP.WebGoat.NET.App_Code.DB;
+using OWASP.WebGoat.NET.WebGoatCoins;
 
 namespace OWASP.WebGoat.NET.WebGoatCoins.Tests
 {
@@ -22,16 +20,17 @@ namespace OWASP.WebGoat.NET.WebGoatCoins.Tests
               .Returns(new[] { "Question?", "Answer" });
             Settings.CurrentDbProvider = db.Object;
 
-            var page = new ForgotPassword();
-            page.txtEmail = new TextBox { Text = "a@b.com" };
-            page.labelQuestion = new Label();
-            page.PanelForgotPasswordStep2 = new Panel();
-            page.PanelForgotPasswordStep3 = new Panel();
+            var page = new ForgotPassword
+            {
+                txtEmail = new TextBox { Text = "a@b.com" },
+                labelQuestion = new Label(),
+                PanelForgotPasswordStep2 = new Panel(),
+                PanelForgotPasswordStep3 = new Panel()
+            };
 
-            var request = new HttpRequest("", "http://localhost/", "");
-            var response = new HttpResponse(new System.IO.StringWriter());
-            var context = new HttpContext(request, response);
-            HttpContext.Current = context;
+            HttpContext.Current = new HttpContext(
+                new HttpRequest("", "http://localhost/", ""),
+                new HttpResponse(new System.IO.StringWriter()));
 
             // Act
             page.ButtonCheckEmail_Click(null, EventArgs.Empty);

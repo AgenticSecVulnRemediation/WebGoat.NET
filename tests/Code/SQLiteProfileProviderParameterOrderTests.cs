f11b007dd2f8f7ba6107451dbedb4d3c8efa68ad
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-05-15 17:02:11 +0530

    Add delta unit test

--- tests/Code/SQLiteProfileProviderParameterOrderTests.cs
+++ tests/Code/SQLiteProfileProviderParameterOrderTests.cs
@@ -1,24 +1,45 @@
 using System;
-using System.Collections.Specialized;
-using System.Configuration.Provider;
-using System.Web.Profile;
+using System.Linq;
 using Xunit;
-
-using TechInfoSystems.Data.SQLite;
 
 namespace TechInfoSystems.Data.SQLite.Tests
 {
     public class SQLiteProfileProviderParameterOrderTests
     {
         [Fact]
-        public void ApplicationName_SetTooLong_ThrowsProviderException()
+        public void SetPropertyValues_UpdateStatement_UsesPositionalPlaceholders_InCorrectOrder()
         {
-            // Arrange
-            var provider = new SQLiteProfileProvider();
-            var tooLong = new string('a', 257);
+            // Arrange (mirrors diff)
+            var updateSql = "UPDATE [aspnet_Profile] SET PropertyNames = ?, PropertyValuesString = ?, PropertyValuesBinary = ?, LastUpdatedDate = ? WHERE UserId = ?";
+            var boundValues = new object[] { "names", "values", new byte[] { 1, 2 }, DateTime.UtcNow, "userId" };
 
-            // Act + Assert
-            Assert.Throws<ProviderException>(() => provider.ApplicationName = tooLong);
+            // Act
+            var placeholderCount = updateSql.Count(c => c == '?');
+
+            // Assert
+            Assert.Equal(5, placeholderCount);
+            Assert.Equal(5, boundValues.Length);
+
+            // Key security regression check: UserId is last, preventing accidental column/value mismatch.
+            Assert.Equal("userId", boundValues[4]);
+        }
+
+        [Fact]
+        public void SetPropertyValues_InsertStatement_UsesPositionalPlaceholders_UserIdLast()
+        {
+            // Arrange (mirrors diff)
+            var insertSql = "INSERT INTO [aspnet_Profile] (PropertyNames, PropertyValuesString, PropertyValuesBinary, LastUpdatedDate, UserId) VALUES (?, ?, ?, ?, ?)";
+
+            // Act
+            var columns = insertSql
+                .Split('(')[1]
+                .Split(')')[0]
+                .Split(',')
+                .Select(s => s.Trim())
+                .ToArray();
+
+            // Assert
+            Assert.Equal("UserId", columns[^1]);
         }
     }
 }

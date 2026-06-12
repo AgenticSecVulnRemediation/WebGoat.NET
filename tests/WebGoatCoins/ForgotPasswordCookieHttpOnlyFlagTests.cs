89a85d42b9369bd9be2bc81f2792a0ebbf800e09
RohasRedz <32270188+RohasRedz@users.noreply.github.com>
2026-06-12 16:37:05 +0530

    Add delta unit test

---
+++ tests/WebGoatCoins/ForgotPasswordCookieHttpOnlyFlagTests.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Web;
+using Xunit;
+
+namespace OWASP.WebGoat.NET.WebGoatCoins.Tests
+{
+    public class ForgotPasswordCookieHttpOnlyFlagTests
+    {
+        [Fact]
+        public void EncryptedSecurityAnswerCookie_IsHttpOnly()
+        {
+            // Arrange
+            var cookie = new HttpCookie("encr_sec_qu_ans");
+
+            // Act
+            cookie.HttpOnly = true;
+
+            // Assert
+            Assert.True(cookie.HttpOnly);
+        }
+    }
+}

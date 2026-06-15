513ebda94f698e9a3585d8e4ede84a4041236a55
RohasRedz <32270188+RohasRedz@users.noreply.github.com>
2026-06-15 13:21:42 +0530

    Add delta unit test

---
+++ tests/App_Code/CookieManagerTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Web;
+using System.Web.Security;
+using OWASP.WebGoat.NET.App_Code;
+using Xunit;
+
+namespace OWASP.WebGoat.NET.App_Code.Tests
+{
+    public class CookieManagerTests
+    {
+        [Fact]
+        public void SetCookie_WhenCalled_SetsHttpOnlyAndSecureFlags()
+        {
+            // Arrange
+            var ticket = new FormsAuthenticationTicket(
+                1,
+                "user",
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(30),
+                isPersistent: false,
+                userData: "data");
+
+            // Act
+            HttpCookie cookie = CookieManager.SetCookie(ticket, "id", "value");
+
+            // Assert
+            Assert.True(cookie.HttpOnly);
+            Assert.True(cookie.Secure);
+            Assert.Equal(FormsAuthentication.FormsCookieName, cookie.Name);
+            Assert.False(string.IsNullOrWhiteSpace(cookie.Value));
+        }
+    }
+}

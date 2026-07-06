08b40167f5fc429a07ba258eb29dbad5a6a4236e
RohasRedz <32270188+RohasRedz@users.noreply.github.com>
2026-07-06 19:07:55 +0530

    Add delta unit test

---
+++ tests/App_Code/DB/SqliteDbProviderGetProductDetailsTests.cs
@@ -0,0 +1,23 @@
+using Moq;
+using Xunit;
+
+namespace OWASP.WebGoat.NET.App_Code.DB.Tests
+{
+    public class SqliteDbProviderGetProductDetailsTests
+    {
+        [Fact]
+        public void GetProductDetails_UsesParameter_ForProductCodeToPreventSqlInjection()
+        {
+            // Arrange
+            var configMock = new Mock<ConfigFile>(MockBehavior.Loose);
+            configMock.Setup(c => c.Get(It.IsAny<string>())).Returns("dummy");
+            var provider = new SqliteDbProvider(configMock.Object);
+
+            // Act
+            var ex = Record.Exception(() => provider.GetProductDetails("S10_1678' OR '1'='1"));
+
+            // Assert
+            Assert.Null(ex);
+        }
+    }
+}
